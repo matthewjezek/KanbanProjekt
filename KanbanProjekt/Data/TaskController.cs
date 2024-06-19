3185724810f5ebd394b6419e689934e55cd84399
matthewjezek <jetam.jezel@gmail.com>
2024-06-19 17:26:43 +0200

    ccolumn circular button -> regular button

---
+++ KanbanProjekt/Data/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanProjekt.Data
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TasksController : ControllerBase
+	{
+		private readonly KanbanKontext _context;
+
+		public TasksController(KanbanKontext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost("updatecolumn")]
+		public IActionResult UpdateColumn(int taskId, int columnId)
+		{
+			var taskItem = _context.TaskItems.Find(taskId);
+			if (taskItem != null)
+			{
+				taskItem.ColumnId = columnId;
+				_context.SaveChanges();
+				return Ok();
+			}
+			return NotFound();
+		}
+	}
+}
